0cc2466c4f17fd48f960482ba5a2adaa6167e243
SimonMoinetUnity <simonmoinet79@gmail.com>
2024-02-07 11:00:23 +0100

    Modification du server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/front'));
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dist/<app-name>/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/front/index.html'));
 });
 
 app.listen(process.env.PORT);
